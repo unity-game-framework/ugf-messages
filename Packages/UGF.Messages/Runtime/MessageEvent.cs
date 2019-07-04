2d4dd547a7b38a3cec99a5d7fd9e6989339f694f
Alexander <vorobyovvorobyov@gmail.com>
2019-07-04 23:14:55 +0300

    Add summaries

--- Packages/UGF.Messages/Runtime/MessageEvent.cs
+++ Packages/UGF.Messages/Runtime/MessageEvent.cs
@@ -3,6 +3,9 @@ using UnityEngine.Events;
 
 namespace UGF.Messages.Runtime
 {
+    /// <summary>
+    /// Represents message without arguments as serializable Unity event.
+    /// </summary>
     [Serializable]
     public class MessageEvent : UnityEvent
     {
